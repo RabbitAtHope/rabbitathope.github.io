0ea38c01cb120697264dcd3f4ef0906c7546961d
Madeleine Rabitoy <159293241+RabbitAtHope@users.noreply.github.com>
2025-02-14 14:06:14 -0500

    Update update-page.py

--- .github/update-page.py
+++ .github/update-page.py
@@ -8,8 +8,8 @@ html = open('index.html', 'r', encoding='utf-8').read()
 start_pos = html.find('\u200B') + len('\u200B') # zero-width spaces
 end_pos = html.find('\u200B', start_pos)
 
-profile = html[:start_pos] + 'Testing...' + html[end_pos:]
+newHtml = html[:start_pos] + 'Testing...' + html[end_pos:]
 
 overwrite = open('index.html', 'w', encoding='utf-8')
-overwrite.write(html)
+overwrite.write(newHtml)
 overwrite.close()
