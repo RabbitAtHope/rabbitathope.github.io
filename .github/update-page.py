6115938a4e697709cb6af1fa6f5405fcb7432bc0
Madeleine Rabitoy <159293241+RabbitAtHope@users.noreply.github.com>
2025-02-14 14:04:52 -0500

    Create update-page.py

---
+++ .github/update-page.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import urllib.request
+
+html = open('index.html', 'r', encoding='utf-8').read()
+start_pos = html.find('\u200B') + len('\u200B') # zero-width spaces
+end_pos = html.find('\u200B', start_pos)
+
+profile = html[:start_pos] + 'Testing...' + html[end_pos:]
+
+overwrite = open('index.html', 'w', encoding='utf-8')
+overwrite.write(html)
+overwrite.close()
