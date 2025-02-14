edc63e17a873c91bcc34fdf4e8f451006a40840c
Madeleine Rabitoy <159293241+RabbitAtHope@users.noreply.github.com>
2025-02-14 14:21:39 -0500

    Update update-page.py

--- .github/update-page.py
+++ .github/update-page.py
@@ -4,15 +4,17 @@ import json
 import os
 import urllib.request
 
-html = open('index.html', 'r', encoding='utf-8').read()
-start_pos = html.find('\u200B') + len('\u200B') # zero-width spaces
-end_pos = html.find('\u200B', start_pos)
+# html = open('index.html', 'r', encoding='utf-8').read()
 
+# Build page content
 pageStart = "<!DOCTYPE html><html><body>"
 pageContent = "<h1>Testing</h1><p>Testing...</p>"
 pageEnd = "</body></html>"
-newHtml = html[:start_pos] + pageStart + pageContent + pageEnd + html[end_pos:]
 
+# Build final page
+newHtml = pageStart + pageContent + pageEnd
+
+# Overwrite the current page contents
 overwrite = open('index.html', 'w', encoding='utf-8')
 overwrite.write(newHtml)
 overwrite.close()
